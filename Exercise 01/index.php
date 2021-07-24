4b0e4eb1bd2dc3a3c7e83182aaa08d38cf8785ee
SithumDev07 <sithum.dashantha@gmail.com>
2021-07-24 23:44:15 +0530

    Exercise 01

---
+++ Exercise 01/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <?php
+
+    $x = 8;
+    $y = 7;
+
+    $z = ($x + $y) * 5;
+
+    echo "The total is " . $z;
+
+    ?>
+
+</body>
+
+</html>
