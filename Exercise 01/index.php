61f0678894cf72e7fc39c500530636f0d2b74d55
SithumDev07 <sithum.dashantha@gmail.com>
2021-07-25 14:24:26 +0530

    Finished Exercise 01

--- Exercise 01/index.php
+++ Exercise 01/index.php
@@ -25,17 +25,61 @@
 
     $z = ($x + $y) * 5;
 
-    echo "The total is " . $z;
+    echo "The total is " . $z . "<br><br>";
 
-    //value Added Text
+    //value Added Tax
 
     $price = 100;
     $vat  = 0.21;
 
 
-    $totalPrice = $price * $vat;
+    $totalPrice = ($price * $vat) + $price;
 
+    echo "Value Added TaxPrice " . $price . "<br>";
+    echo "Vat " . $vat . "<br>";
+    echo "Total Price " . $totalPrice . "<br><br>";
 
+    //Average
+
+    $x = 4;
+    $y = 5;
+    $z = 2;
+
+    $average = ($x + $y + $z) / 3;
+
+    echo "Average " . number_format($average, 2, ',', ' ') . "<br><br>";
+
+    $countries = array("Netherlands" => "Amsterdam", "Germany" => "Berlin", "Thailand" => "Bangkok", "Sri Lanka" => "Colombo", "Canada" => "Toronto");
+
+    foreach ($countries as $key => $value) {
+        echo "The capital of  " . $key . " is " . $value;
+        echo "<br>";
+    }
+
+    echo "<br><br>";
+
+    $cm = 150;
+
+    $cmToInch = $cm * 0.39;
+
+    echo $cm . " centimeters is " . $cmToInch . " inch <br><br>";
+
+    $expenses = array(40, 70, 85, 150);
+
+    $totalAmount;
+    $amountOfExpenses;
+
+    foreach ($expenses as $expense) {
+        $totalAmount = $totalAmount + $expense;
+    }
+
+    echo "The total amount " . $totalAmount . "<br><br>";
+
+    //Weather Conditions
+
+    $weather = array("Rain", "Sunshine", "Clouds", "Hail", "Sleet", "Snow", "Wind");
+
+    echo "We've seen all kinds of weather this month. At the begining of the month, we had " . $weather[5] . " and " . $weather[6] . ". Then came " . $weather[1] . " with a few " . $weather[2] . " and some " . $weather[0] . ". At least we didn't get any " . $weather[3] . " or " . $weather[4];
 
     ?>
 
