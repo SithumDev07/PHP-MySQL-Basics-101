8fa31198f03583ac0ec7b8e51178cc82824a10b6
SithumDev07 <sithum.dashantha@gmail.com>
2021-07-27 13:50:26 +0530

    Basic Functions

---
+++ 06/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            background-color: #393939;
+            color: #fff;
+            font-family: sans-serif;
+            font-size: 24px;
+        }
+    </style>
+</head>
+<?php
+//Built in functions
+//Ceil
+echo ceil(1.50) . "<br>";
+
+//Floor
+echo floor(1.50) . "<br>";
+
+//Round
+echo round(0.541125, 2) . "<br>";
+
+//Exp
+echo exp(4) . "<br>";
+
+?>
+
+<body>
+
+</body>
+
+</html>
