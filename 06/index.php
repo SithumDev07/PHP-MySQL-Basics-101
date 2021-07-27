1ba68836f364c6a6b020e2003c93ea94b3a6ebad
SithumDev07 <sithum.dashantha@gmail.com>
2021-07-27 14:05:56 +0530

    Array Built in Functions

--- 06/index.php
+++ 06/index.php
@@ -29,6 +29,70 @@ echo round(0.541125, 2) . "<br>";
 //Exp
 echo exp(4) . "<br>";
 
+//String functions
+//Use strlen rather than using maxlength in HTML: Avoid Hackers
+
+$str = "Today is a sunny day";
+
+echo strlen($str) . "<br><br>";
+
+//String word count
+$firstName = "My name is Sithum.";
+echo str_word_count($firstName) . "<br><br>";
+
+//How to find a text within a text.
+$email = "sithum@uxdesino.com";
+if (strpos($email, "@"))
+    echo "This is a valid email.<br><br>";
+else
+    echo "This is not a valid email.<br><br>";
+
+//Ucwords - Uppercase
+echo ucwords("What is your name?" . "<br><br>");
+
+//Whole string to uppercase
+echo strtoupper("What is your name?" . "<br><br>");
+
+//Whole string to lowercase
+echo strtolower("What is your name?" . "<br><br>");
+
+
+//Array built in functions
+
+//Merging arrays
+$personalInfo = array("Name" => "Sithum", "Age" => 21, "City" => "Badulla");
+
+$moreInfo = array("State" => "UVA", "Weight" => 80);
+
+$personalInfo = array_merge($personalInfo, $moreInfo);
+
+foreach ($personalInfo as $key => $value) {
+    echo $key . ": " . $value . "<br>";
+}
+
+//Print only keys
+print_r(array_keys($personalInfo));
+echo "<br><br>";
+
+// Print only values
+print_r(array_values($personalInfo));
+echo "<br><br>";
+
+//Push to arrays
+$cars = array("Volswagen", "Audi", "Mercedes");
+array_push($cars, "Volvo");
+
+print_r($cars);
+echo "<br><br>";
+
+//Array Search
+echo array_search("Volvo", $cars);
+echo "<br><br>";
+
+//Array Count
+echo count($cars);
+echo "<br><br>";
+
 ?>
 
 <body>
