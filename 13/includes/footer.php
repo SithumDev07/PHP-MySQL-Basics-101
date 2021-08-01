01fd580869ce2c13916b3d39756e3874c97cbff7
SithumDev07 <sithum.dashantha@gmail.com>
2021-08-01 15:41:12 +0530

    Started Working in Login And Register and Basic Styles

---
+++ 13/includes/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <p>All rights reserved &copy; | 2021</p>
+</footer>
+
+</body>
+
+</html>
--- 13/index.php
+++ 13/index.php
@@ -1,35 +1,13 @@
 <?php
-require_once 'includes/database.php'
+
+require_once 'includes/header.php';
+
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+Home
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
+<?php
 
-<body>
+require_once 'includes/footer.php';
 
-    <?php
-
-    $sql = "SELECT * FROM users where id=1";
-    $result = mysqli_query($conn, $sql);
-
-    $rowCount = mysqli_num_rows($result);
-
-    if ($rowCount > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo $row['username'] . $row['id'] . "<br>";
-        }
-    } else {
-        echo 'No results found!';
-    }
-    ?>
-
-</body>
-
-</html>
+?>
